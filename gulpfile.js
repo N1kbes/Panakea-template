4c8a0e29a4bd662c4ae24548f320ed6b05a1d64d
N1kbes <vanya1bes@gmail.com>
2017-01-28 10:13:50 +0200

    Setting up gul with server and livereload

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    connect = require('gulp-connect'),
+    concat = require('gulp-concat'),
+    sass = require('gulp-sass');
+
+gulp.task('connect', function() {
+  connect.server({
+    port: 8080,
+    root: 'src',
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./src/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('css', function () {
+  gulp.src('./src/css/*.css')
+    .pipe(connect.reload());
+});
+
+gulp.task('sass', function () {
+  return gulp.src('./src/sass/*.scss')
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(gulp.dest('./src/css'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/*.html'], ['html']);
+  gulp.watch('./src/sass/*.scss', ['sass']);
+  gulp.watch('./src/css/*.css', ['css']);
+});
+
+gulp.task('default', ['connect', 'watch']);
